ca8918df362a3af4480765b37e0c64c49b99e3f9
Vytas Kancleris <2102204+VytasKan@users.noreply.github.com>
2026-08-18 19:08:30 +0100

    Add chart demos to registry and update imports

--- fallback/registry.ts
+++ fallback/registry.ts
@@ -1,11 +1,16 @@
 "use client";
 import { dynamic } from "@repo/ui/elements/dynamic";
- 
+
+import { ChartjsDemo } from "@repo/chartjs/demo";
+import { GoogleChartDemo } from "@repo/google-charts/demo";
+import { LWChartDemo } from "@repo/lw-charts/demo";
+import { MuiChartsDemo } from "@repo/mui-charts/demo";
+import { PlotlyDemo } from "@repo/plotly/demo";
+import { RechartDemo } from "@repo/recharts/demo";
 
 export const COMPONENTS = {
 	Marketing: dynamic(() => import("@repo/marketing")),
 
- 
 	LWYieldChart: dynamic(() => import("@repo/lw-charts/yield-curve-chart")),
 	LWOptionsChart: dynamic(() => import("@repo/lw-charts/options-chart")),
 	TWWidget: dynamic(() => import("@repo/lw-charts/widget")),
@@ -23,13 +28,22 @@ export const COMPONENTS = {
 	MonacoEditor: dynamic(() => import("@repo/monaco")),
 
 	NotificationToolbarAction: dynamic(() => import("@repo/notifications")),
-	DataForm: dynamic(() => import("@repo/forms"))
+	DataForm: dynamic(() => import("@repo/forms")),
 };
-export const ICONS = {
-	 
+
+export const ICONS = {};
+
+export const CHART_DEMOS = {
+	GoogleChartDemo,
+	ChartjsDemo,
+	MuiChartsDemo,
+	RechartDemo,
+	LWChartDemo,
+	PlotlyDemo,
 };
 
 export const registry = {
 	components: COMPONENTS,
 	icons: ICONS,
+	chartDemos: CHART_DEMOS,
 };
